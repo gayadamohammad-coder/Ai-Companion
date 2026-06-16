9cf296ac3e6ba55895e1166b39078877248dae99
Mohammad Gayada <gayadamohammad7@gmail.com>
2026-06-16 07:56:27 +0300

    Create flask website

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return """
-    <h1>AI Companion</h1>
-    <p>Welcome Mohammed!</p>
-    """
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
